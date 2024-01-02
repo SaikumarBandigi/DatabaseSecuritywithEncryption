c021ce9db4630b431efc4aa9e9ab7cbb4bb65233
Saikumar Bandigi <bandigisaikumar@gmail.com>
2024-01-02 19:02:48 +0530

    Initial commit

---
+++ src/main/java/com/springboot/CustomSecurityWithDB/BCryptPasswordEncoderTest.java
@@ -0,0 +1,18 @@
+package com.springboot.CustomSecurityWithDB;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BCryptPasswordEncoderTest {
+
+	public static void main(String[] args) {
+
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+		System.out.println("Password for Swetha...... " +bCryptPasswordEncoder.encode("Sonu"));
+		System.out.println("Password for Harsha...... " +bCryptPasswordEncoder.encode("Ratna"));
+
+		//$2a$10$6w6rjJXJMwIs0Cn9psNyu.Zjo1e18754Wjsxiev.PDAlbO1dlbuDu
+		//$2a$10$AMN7cfdX1pRTp.SCGAEgTu2JH6AAmjt/.MoGx3bJ8WLK6gbJMdcvK
+		//System.out.println("Password for Guest...... " +bCryptPasswordEncoder.encode("guest"));
+	}
+}
